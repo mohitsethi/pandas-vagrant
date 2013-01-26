3f9f370a749ff9d3955a9e2cbbe2bda6483bafa5
mohitsethi <mohit@sethis.in>
2013-01-26 09:45:59 +0530

    Added cookbook:pandas

---
+++ cookbooks/pandas/metadata.rb
@@ -0,0 +1,7 @@
+name             'pandas'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'mohit@sethis.in'
+license          'All rights reserved'
+description      'Installs/Configures pandas'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
